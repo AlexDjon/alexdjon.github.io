dfb40da962e70b556e0ded99bce46efae042843a
AlexDjon <alexdjonata30@gmail.com>
2020-08-13 14:44:10 -0300

    Change Color Aside

--- wiki-relayout/js/scripts.js
+++ wiki-relayout/js/scripts.js
@@ -2,12 +2,20 @@ function getItem (info) {
   return document.querySelector(info)
 }
 
+function getAll(info) {
+  return document.querySelectorAll(info)
+}
+
 getItem('.user').addEventListener('click', () => {
   getItem('.areas').classList.toggle('ativo')
 })
 
-getItem('.navbar .close').addEventListener('click', function () {
-  
+
+
+/*
+  * CLOSE NAVBAR
+*/
+getItem('.navbar .close').addEventListener('click', function () {  
   getItem('.navbar').classList.toggle('ativo')
 
   const thisIcon = (context, value, action = 'get') => {
@@ -23,13 +31,46 @@ getItem('.navbar .close').addEventListener('click', function () {
 
   if (icon === 'chevron_left') {
     this.style.left = '0px'
+    getItem('.navbar .menu').style.display = 'none'
     thisIcon(this, 'chevron_right', 'change')
+
     getItem('.meio').style.width = '100%'
   }
-  else {
+  else 
+  {
     this.style.left = '205px'
+    getItem('.navbar .menu').style.display = 'flex'
     thisIcon(this, 'chevron_left', 'change')
 
     getItem('.meio').style.width = 'calc(100% - var(--navX)'
   }
 })
+
+// document.documentElement.style.setProperty('--verdeW', 'red')
+function closeAll() {
+  const all = getAll('.viewer')
+
+  all.forEach(e => {
+    e.style.display = 'none'
+  })
+}
+
+function changeLocal(newLocal) {
+  closeAll()
+
+  getItem(`.viewer#${newLocal}`).style.display = 'block'
+}
+
+
+function changeColor (loader) {
+  const varCss = getItem('#local').value
+
+  const styles = getComputedStyle(document.documentElement);
+
+  if(loader == 'select') {
+    getItem('#novaCor').value = String(styles.getPropertyValue(varCss)).trim()
+  }
+  const color = getItem('#novaCor').value
+
+  document.documentElement.style.setProperty(varCss, color)
+}
